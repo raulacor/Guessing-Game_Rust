4ff80ad3fcc9b5c43cdd88c9d7aada54dfa77a21
Raul Azzi Corsi <raulcorsi@Rauls-MacBook.local>
2025-09-13 13:23:23 -0300

    matching guessed with seret number using Ordering

---
+++ target/debug/deps/rand_chacha-dd4996524ffa163d.d
@@ -0,0 +1,7 @@
+/Users/raulcorsi/repos/Rust/GuessingGame/target/debug/deps/rand_chacha-dd4996524ffa163d.d: /Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/lib.rs /Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/chacha.rs /Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/guts.rs
+
+/Users/raulcorsi/repos/Rust/GuessingGame/target/debug/deps/librand_chacha-dd4996524ffa163d.rmeta: /Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/lib.rs /Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/chacha.rs /Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/guts.rs
+
+/Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/lib.rs:
+/Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/chacha.rs:
+/Users/raulcorsi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.9.0/src/guts.rs:
