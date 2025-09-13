184993a25cf8a93324f985f1b2faf39672093bf9
Raul Azzi Corsi <raulcorsi@Rauls-MacBook.local>
2025-09-13 13:58:23 -0300

    Fix: removed submodule and added folder contents

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("Welcome to the guesing game");
+
+    loop {
+        println!("Please input your number:");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input"); 
+                continue;
+            }
+        };
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You got it!");
+                break;
+            }
+        }
+    }
+}
