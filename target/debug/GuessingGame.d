4f13a0d423859c6e6116633f4b98176f1597df73
Raul Azzi Corsi <raulcorsi@Rauls-MacBook.local>
2025-09-13 12:50:31 -0300

    Random number gen. with rand

---
+++ target/debug/GuessingGame.d
@@ -0,0 +1,1 @@
+/Users/raulcorsi/repos/Rust/GuessingGame/target/debug/GuessingGame: /Users/raulcorsi/repos/Rust/GuessingGame/src/main.rs
---
+++ target/debug/deps/GuessingGame-93274d66262ae1e5.d
@@ -0,0 +1,5 @@
+/Users/raulcorsi/repos/Rust/GuessingGame/target/debug/deps/GuessingGame-93274d66262ae1e5.d: src/main.rs
+
+/Users/raulcorsi/repos/Rust/GuessingGame/target/debug/deps/GuessingGame-93274d66262ae1e5: src/main.rs
+
+src/main.rs:
